3d7b3112b11c98ff4f401cb0e016987422972f21
rana <rana@ranas-MacBook-Pro.local>
2022-04-08 23:15:00 +0600

    vector icon added

--- src/navigation/BottomTabNavigator.tsx
+++ src/navigation/BottomTabNavigator.tsx
@@ -1,6 +1,7 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 //screens
 import HomeScreen from './../screens/HomeScreen';
@@ -15,11 +16,23 @@ type Props = {}
 
 const BottomTabNavigator = (props: Props) => {
     return (
-        <Tab.Navigator>
-            <Tab.Screen name='Home' component={HomeScreen} />
-            <Tab.Screen name='Search' component={SearchScreen} />
-            <Tab.Screen name='Favorite' component={FavoriteScreen} />
-            <Tab.Screen name='Offer' component={OfferScreen} />
+        <Tab.Navigator
+            screenOptions={{
+                tabBarShowLabel: false
+            }}
+        >
+            <Tab.Screen name='Home' component={HomeScreen} options={{
+                tabBarIcon: (props) => <Icon name={props.focused ? "home" : "home-outline"} size={22} />
+            }} />
+            <Tab.Screen name='Search' component={SearchScreen} options={{
+                tabBarIcon: (props) => <Icon name={props.focused ? "search" : "search-outline"} size={22} />
+            }} />
+            <Tab.Screen name='Favorite' component={FavoriteScreen} options={{
+                tabBarIcon: (props) => <Icon name={props.focused ? "heart" : "heart-outline"} size={22} />
+            }} />
+            <Tab.Screen name='Offer' component={OfferScreen} options={{
+                tabBarIcon: (props) => <Icon name={props.focused ? "pricetag" : "pricetag-outline"} size={22} />
+            }} />
         </Tab.Navigator>
     )
 }
